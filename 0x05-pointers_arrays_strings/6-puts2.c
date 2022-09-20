7914be16d50e6628af9f228ca26fdaab71b0a3d9
Dus John <dusjohn02@gmail.com>
2022-09-19 19:52:36 -0700

    Do task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every caharacter of a string
+ * starting with the first guy
+ *
+ * @str: The input string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
