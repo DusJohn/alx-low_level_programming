93d39d951c397c5dd16655352facd114c6d863ff
Dus John <dusjohn02@gmail.com>
2022-09-19 17:42:39 -0700

    Do task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of n to 98
+ *
+ * @n: This is a pointer variable that points to an integer
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+#endif
