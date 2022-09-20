4294f764c5c411c507b608e395d807ba2f669491
Dus John <dusjohn02@gmail.com>
2022-09-19 19:24:04 -0700

    Correct task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string (char *s)
 	int len, i, half;
 	char temp;
 
-	for (len = 0; s[lenn] != '\0'; len ++);
+	for (len = 0; s[len] != '\0'; len ++);
 	i = 0;
 
 	half = len / 2;
@@ -25,4 +25,4 @@ void rev_string (char *s)
 		s[i] = temp;
 		i++;
 	}
-}
+i}
