4a42f8675fd714ec8070aa9b2fcde92ee0cc96c8
Dus John <dusjohn02@gmail.com>
2022-09-22 16:22:55 -0700

    do task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - This function compares two strings.
+ *
+ * @s!: String 1.
+ *
+ * @s@: String 2.
+ *
+ * Return: 0 if the compared strings are similar, and ns1 - ns2 if they are
+ * dissimilar.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i= 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
